a9d179d28da9c249f5b9aeaa514433dec5530f2f
“[Krishnakant-pal]” <krishnakant.pal127@gmail.com>
2023-11-19 22:24:17 +0530

    Added data tranformation

--- src/DimondPricePrediction/pipelines/training_pipeline.py
+++ src/DimondPricePrediction/pipelines/training_pipeline.py
@@ -11,3 +11,10 @@ import pandas as pd
 obj=DataIngestion()
 
 obj.initiate_data_ingestion()
+
+train_data_path = obj.ingestion_config.train_data_path
+test_data_path = obj.ingestion_config.test_data_path
+
+data_transformation=DataTransformation()
+
+train_arr,test_arr=data_transformation.initialize_data_transformation(train_data_path,test_data_path)
