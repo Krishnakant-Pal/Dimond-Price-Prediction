0a1638b30354d16348a9130f4ca32b6c440490a3
“[Krishnakant-pal]” <krishnakant.pal127@gmail.com>
2023-11-25 20:10:11 +0530

    Added model trainer

--- src/DimondPricePrediction/pipelines/training_pipeline.py
+++ src/DimondPricePrediction/pipelines/training_pipeline.py
@@ -1,6 +1,6 @@
 from src.DimondPricePrediction.components.data_ingestion import DataIngestion
-
 from src.DimondPricePrediction.components.data_transformation import DataTransformation
+from src.DimondPricePrediction.components.model_trainer import ModelTrainer
 
 import os
 import sys
@@ -18,3 +18,6 @@ test_data_path = obj.ingestion_config.test_data_path
 data_transformation=DataTransformation()
 
 train_arr,test_arr=data_transformation.initialize_data_transformation(train_data_path,test_data_path)
+
+model_trainer_obj= ModelTrainer()
+model_trainer_obj.initate_model_training(train_arr,test_arr)
